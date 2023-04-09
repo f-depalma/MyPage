b3390f00b9d3e8e17a1fc6042b9d0670950d27ae
f.depalma <325587@via.dk>
2023-04-09 09:56:08 +0200

    place holder

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Hello Walid!
-      </header>
+    <div className='flex flex-col items-center justify-center h-screen text-gray-200'>
+      <h1 className='text-7xl'>Work in progress!</h1>
+      <br/>
+      <h4 className='text-3xl'>Happy Easter 🐣</h4>
     </div>
   );
 }
