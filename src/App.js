30ad805d1df611e73b795b4da83bd926b77f8bfa
f.depalma <325587@via.dk>
2023-04-09 10:24:38 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className='flex flex-col items-center justify-center h-screen text-gray-200'>
-      <img src='face-in-black.jpg' className='face-image z-0'/>
+      <img src='face-in-black.jpg' className='face-image z-0 object-contain'/>
       <h1 className='md:text-7xl text-4xl z-10'>Work in progress!</h1>
       <br/>
       <h4 className='md:text-3xl text-xl z-10'>Happy Easter 🐣</h4>
