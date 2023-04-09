6f454e3d43bf3f8b2a025b32de042146467289c5
f.depalma <325587@via.dk>
2023-04-09 19:57:53 +0200

    fix section 2 carosel

--- src/component/Section2.js
+++ src/component/Section2.js
@@ -3,12 +3,14 @@ import Skill from "./Skill"
 
 export default function Section2() {
 
-    const skillComponents = skills.map((skill, idx) => <Skill key={idx} props={skill}/>)
-    
+    const skillComponents = skills.map((skill, idx) => <Skill key={idx} props={skill} />)
+
     return (
         <div className="bg-white text-neutral-800 flex justify-center">
-            <div className="flex flex-row items-center justify-around flex-wrap h-screen w-10/12">
-                {skillComponents}
+            <div className="w-10/12 overflow-x-scroll flex justify-start pr-0">
+                <div className="w-fit flex flex-row justify-center items-center h-screen  m-auto">
+                    {skillComponents}
+                </div>
             </div>
         </div>
     )
--- src/component/Skill.js
+++ src/component/Skill.js
@@ -1,8 +1,8 @@
 export default function Skill({ props: { icon, name, description } }) {
     return (
-        <div className="min-w-32 max-w-32 px-6">
+        <div className="skill w-80 px-6">
             <img src={icon} className="pb-4 w-32 m-auto" />
-            <h4 className="text-center text-xl font-bold pb-2">{name}</h4>
+            <h4 className="text-center text-xl font-bold pb-2 text-zinc-700">{name}</h4>
             <p className="text-center">{description}</p>
         </div>
     )
