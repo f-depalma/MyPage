50085c6380730c118e0c1b96b33abdc288831e9a
f.depalma <325587@via.dk>
2023-04-09 10:18:09 +0200

    responsive

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className='flex flex-col items-center justify-center h-screen text-gray-200'>
-      <h1 className='text-7xl'>Work in progress!</h1>
+      <img src='face-in-black.jpg' className='face-image z-0'/>
+      <h1 className='md:text-7xl text-4xl z-10'>Work in progress!</h1>
       <br/>
-      <h4 className='text-3xl'>Happy Easter 🐣</h4>
+      <h4 className='md:text-3xl text-xl z-10'>Happy Easter 🐣</h4>
     </div>
   );
 }
