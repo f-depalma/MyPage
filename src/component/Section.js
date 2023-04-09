72ef495b10af729f2ee5c20a7eb7bbedcafa8b0d
f.depalma <325587@via.dk>
2023-04-09 17:44:09 +0200

    fix scroll

--- src/component/Section.js
+++ src/component/Section.js
@@ -1,19 +1,10 @@
-import { useEffect, useState } from "react"
-import useScrollPosition from "../hooks/scroll-position"
 
-export default function Section({id, section, children}) {
-    const [visible, setVisible] = useState('section-hide')
-
-    useEffect(() => {
-        if (section === id) {
-            setTimeout(() => setVisible('section-visible'))
-        }
-    })
-
-    const scrollPosition = useScrollPosition();
-
+export default function Section({ visibility, children }) {
     return (
-        <section className={`${visible} + section h-screen w-screen`}>
+        <section
+            className="section h-screen w-screen"
+            style={visibility.getCss()}
+        >
             {children}
         </section>
     )
