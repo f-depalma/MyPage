e3dd805c67f80336689053c60a3ccda37a373e8a
f.depalma <325587@via.dk>
2023-04-09 20:26:10 +0200

    fix scroll experience

--- src/component/Content.js
+++ src/component/Content.js
@@ -2,13 +2,15 @@ import { useWindowHeight } from "../hooks/window-height"
 import Section from "./Section"
 import Section1 from "./Section1"
 import Section2 from "./Section2"
+import Section3 from "./Section3"
 import { useVisibilities } from "../hooks/visibilities"
 
 export default function Content() {
 
     const sections = [
         <Section1 />,
-        <Section2 />
+        <Section2 />,
+        // <Section3 />
     ]
 
     const screenHeight = useWindowHeight()
