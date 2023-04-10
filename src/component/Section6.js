dc285072e054fca75180b7b89dc7025d0639c539
f.depalma <325587@via.dk>
2023-04-10 22:53:32 +0200

    fix responsiveness

--- src/component/Section6.js
+++ src/component/Section6.js
@@ -1,10 +1,10 @@
 export default function Section6() {
     return (
-        <div className="footer flex flex-col justify-center h-full pl-20 text-white">
+        <div className="footer flex flex-col justify-center h-full px-20 text-white items-center md:items-start text-center">
             <h1 className="text-4xl mb-8">WANT TO TALK WITH ME?</h1>
             <div className="h-0 w-24 border-2 border-white mb-8"></div>
             <p className="text-lg mb-4">I'd love to find out more about your projects. Get in touch now.</p>
-            <div className="bg-black h-14 w-44 flex flex-row justify-center items-center rounded">
+            <div className="bg-black h-14 w-44 flex flex-row justify-center items-center rounded hover:bg-zinc-800 hover:text-yellow-500 hover:cursor-pointer">
                 <span>CONTACT ME</span>
             </div>
         </div>
