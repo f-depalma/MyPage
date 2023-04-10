8c1b81921847ae24aab1e4895d2f2aed5b380862
f.depalma <325587@via.dk>
2023-04-10 13:36:44 +0200

    fix responsiveness

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,10 +1,11 @@
 export default function Header() {
     return (
-        <nav id="header" className="flex flex-row justify-between bg-black z-50">
-            <img src='logo.png' className="p-4"></img>
+        <nav id="header" className="h-28 sm:h-24 flex flex-col sm:flex-row sm:justify-between bg-black z-50">
+            <img src='logo.png' className="p-4 h-20 sm:h-24 object-contain"></img>
             <div className="flex flex-row justify-between items-center">
-                <div className="pr-8 h-8 nav--item hover:text-gray-400 hover:cursor-pointer">My Projects</div>
-                <div className="pr-12 h-8 nav--item hover:text-gray-400 hover:cursor-pointer">Contact Me</div>
+                <div className="px-8 h-8 nav--item hover:text-gray-400 hover:cursor-pointer text-xs sm:text-base">My Projects</div>
+                <div className="px-8 h-8 nav--item hover:text-gray-400 hover:cursor-pointer text-xs sm:text-base">Others</div>
+                <div className="px-8 h-8 nav--item hover:text-gray-400 hover:cursor-pointer text-xs sm:text-base">Contact Me</div>
             </div>
         </nav>
     )
