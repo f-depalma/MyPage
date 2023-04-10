c4855a0671bdd1c86991f2ea1290db38684b1afa
f.depalma <325587@via.dk>
2023-04-10 22:47:43 +0200

    add footer

--- src/component/Content.js
+++ src/component/Content.js
@@ -6,6 +6,7 @@ import Section3 from "./Section3"
 import Section4 from "./section4"
 import { useVisibilities } from "../hooks/visibilities"
 import Section5 from "./Section5"
+import Section6 from "./Section6"
 
 export default function Content() {
 
@@ -14,7 +15,8 @@ export default function Content() {
         <Section2 />,
         <Section3 />,
         <Section4 />,
-        <Section5 />
+        <Section5 />,
+        <Section6 />
     ]
 
     const screenHeight = useWindowHeight()
---
+++ src/component/Section6.js
@@ -0,0 +1,12 @@
+export default function Section6() {
+    return (
+        <div className="footer flex flex-col justify-center h-full pl-20 text-white">
+            <h1 className="text-4xl mb-8">WANT TO TALK WITH ME?</h1>
+            <div className="h-0 w-24 border-2 border-white mb-8"></div>
+            <p className="text-lg mb-4">I'd love to find out more about your projects. Get in touch now.</p>
+            <div className="bg-black h-14 w-44 flex flex-row justify-center items-center rounded">
+                <span>CONTACT ME</span>
+            </div>
+        </div>
+    )
+}
